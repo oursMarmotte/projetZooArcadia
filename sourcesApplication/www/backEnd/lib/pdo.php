fcf347168402391e8ea0c23dc0d0722c1bc5ee4b
oursMarmotte <149693469+oursMarmotte@users.noreply.github.com>
2024-11-10 15:53:01 +0100

    Update pdo.php
    mis a jour le 101124

--- sourcesApplication/www/backEnd/lib/pdo.php
+++ sourcesApplication/www/backEnd/lib/pdo.php
@@ -3,7 +3,7 @@ use FFI\Exception;
 
 
 try{ 
-            $pdo = new PDO('mysql:host=mysql-centserv.alwaysdata.net;dbname=centserv_zooarcadia','centserv','leviathan@472');    
+            $pdo = new PDO('mysql:host=mysql-centserv.alwaysdata.net;dbname=centserv_zooarcadia','','');    
 
    
 
